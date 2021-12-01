68843e3dc559663ead3dce0ef6f691416f1420d3
bakkar3 <amarbakar33@gmail.com>
2021-12-01 09:46:51 +0100

    session

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
- 
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 const PORT = 5001;
- 
+
+app.use(cookieParser());
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "secret"
+    })
+);
+
 app.get("/test", (req, res) => {
     res.send('this is an endpoint test');
 });
  
-app.listen(PORT, (req, res) => {
-    console.log(`API listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`API started on http://localhost:${PORT}`));
